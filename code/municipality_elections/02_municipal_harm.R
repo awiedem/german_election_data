9dec56d12e0a06d8349654a28da2a0366069e8ba
vincentheddesheimer <86489953+vincentheddesheimer@users.noreply.github.com>
2024-09-27 17:15:41 -0400

    Update harm

--- code/municipality_elections/02_municipal_harm.R
+++ code/municipality_elections/02_municipal_harm.R
@@ -451,10 +451,7 @@ table(df_harm$year)
 plot_df <- df_final |>
   group_by(state_name, year) |>
   summarise(election_bin = max(election_bin, na.rm = TRUE)) |>
-  ungroup() |>
-  add_row(
-    state_name = "Bavaria", year = 1990, election_bin = 2
-    )
+  ungroup()
 
 plot_df |>
   ggplot(aes(x = as.numeric(year), 
@@ -481,6 +478,53 @@ plot_df |>
 ggsave("output/figures/muni_elections.pdf", width = 7, height = 4)
 
 move_plots_to_overleaf("code")
+
+
+
+# Check missigness --------------------------------------------------------
+
+
+# Load municipality level data
+muni <- read_rds("data/covars_municipality/final/ags_area_pop_emp.rds") |>
+  rename(ags = ags_21) |>
+  mutate(ags = pad_zero_conditional(ags, 7))
+
+df_harm <- read_rds("data/municipal_elections/final/municipal_harm.rds")
+
+# Merge
+df_final <- muni |>
+  left_join_check_obs(df_harm, by = c("ags", "year")) |>
+  mutate(
+    state = substr(ags, 1, 2),
+    state = pad_zero_conditional(state, 1),
+    state_name = state_id_to_names(state)
+  )
+
+# Create variable indicating whether there was election in given year
+df_final <- df_final |>
+  # election == 1 if any ags in a state and year has election
+  group_by(state, year) |>
+  mutate(
+    election = ifelse(any(!is.na(turnout)), 1, 0)
+  ) |>
+  ungroup()
+  
+# Filter all ags where election == 1 but turnout is missing and that have other observations with non-missing turnout
+ever_election <- df_final |>
+  filter(!is.na(turnout) == 1) |>
+  distinct(ags)
+
+# Filter all ags where election == 1 but turnout is missing and that have other observations with non-missing turnout
+inspection <- df_final |>
+  filter(election == 1 & is.na(turnout) == 1) |>
+  filter(ags %in% ever_election$ags | population_ags > 1) |>
+  mutate(state = state_id_to_names(state)) |>
+  select(ags, ags_name_21, year, state, population_ags, turnout) |>
+  arrange(state, ags, year)
+  
+fwrite(inspection, "data/municipal_elections/missing_ags.csv")
+
+table(inspection$year, inspection$state, useNA = "ifany")
 
 
 ### END
