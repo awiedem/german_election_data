5a381974293f926e60ee69ad3d500946645ef216
vincentheddesheimer <86489953+vincentheddesheimer@users.noreply.github.com>
2024-12-17 12:02:56 -0500

    Partcodes

--- code/crosswalks/03_partycodes.R
+++ code/crosswalks/03_partycodes.R
@@ -86,3 +86,90 @@ glimpse(crosswalk)
 write_rds(crosswalk, "data/crosswalks/party_crosswalk.rds")
 
 ### END
+
+
+
+# Party facts data --------------------------------------------------------
+
+# Get it from: https://partyfacts.herokuapp.com/
+# download and read Party Facts mapping table
+# file_name <- "partyfacts-mapping.csv"
+# if( ! file_name %in% list.files()) {
+#   url <- "https://partyfacts.herokuapp.com/download/external-parties-csv/"
+#   download.file(url, file_name)
+# }
+# partyfacts_raw <- read_csv(file_name, guess_max = 50000)
+
+# partyfacts_ger <- partyfacts_raw %>% 
+#   dplyr::filter(country == "DEU")
+
+# glimpse(partyfacts_ger)
+# table(partyfacts_ger$dataset_key)
+# table(partyfacts_ger$name_short)
+
+# afd <- partyfacts_ger %>% 
+#   dplyr::filter(partyfacts_id == "1976")
+# View(afd)
+
+
+# partyfacts_reduced <- partyfacts_ger %>% 
+#   select(partyfacts_id, name_short, name, name_english) %>% 
+#   mutate(
+#     name_short = str_to_lower(name_short),
+#     name = str_to_lower(name),
+#     name_english = str_to_lower(name_english)
+#   ) %>% 
+#   distinct() %>% 
+#   arrange(partyfacts_id) %>% 
+#   print(n = 600)
+
+# View(partyfacts_reduced)
+
+# partyfacts_reduced <- partyfacts_ger %>% 
+#   select(partyfacts_id, name_short) %>% 
+#   mutate(name_short = str_to_lower(name_short)) %>% 
+#   distinct() %>% 
+#   arrange(partyfacts_id) %>% 
+#   print(n = 600)
+
+
+# crosswalk <- parties %>%
+#   mutate(
+#     partyfacts_id = case_when(
+#       # Major established parties:
+#       party == "afd" ~ 1976,
+#       party %in% c("b90_gr", "gruene", "grune") ~ 1816,  # Bündnis 90/Die Grünen
+#       party %in% c("die_linke", "linke_pds", "pds") ~ 1545,
+#       party == "fdp" ~ 573,
+#       party == "cdu" ~ 1375,
+#       party == "csu" ~ 1731,
+#       party == "spd" ~ 383,
+#       party == "ssw" ~ 50,
+#       party == "piraten" ~ 1287,
+#       party == "diebasis" ~ 8993,
+#       party == "die_partei" ~ 6126,
+#       party == "die_grauen" ~ 1652,
+#       party == "dkp" ~ 1135,
+#       party == "kpd" ~ 1135,    # Both mapped to KPD/DKP ID
+#       party == "freie_wahler" ~ 1090,
+#       party == "odp" ~ 7345,
+#       party == "zentrum" ~ 1798,
+#       party == "npd" ~ 1723,
+#       party == "dvu" ~ 695,
+#       party == "rep" ~ 720,
+      
+#       # Tierschutz
+#       party %in% c("tierschutzpartei", "tierschutz", "die_tierschutzpartei") ~ 749,
+      
+#       # Minor or newer parties appearing under 2702 (based on provided snippet):
+#       # Note: This is tentative. If you have official mappings, use them instead.
+#       party == "mlpd" ~ 2702,
+#       party == "familie" ~ 2702,
+#       party == "lkr" ~ 2702,
+      
+#       # Parties not found or no clear mapping:
+#       TRUE ~ NA_real_
+#     )
+#   )
+
+# crosswalk
